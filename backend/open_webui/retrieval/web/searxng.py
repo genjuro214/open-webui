62db29d55ea91f76e684a9c28df1f6602e7d5ffb
leilibj <genjuro214@126.com>
2025-03-10 15:42:12 +0800

    Update searxng.py

--- backend/open_webui/retrieval/web/searxng.py
+++ backend/open_webui/retrieval/web/searxng.py
@@ -1,12 +1,96 @@
 import logging
 from typing import Optional
+from urllib.parse import urlencode
 
 import requests
 from open_webui.retrieval.web.main import SearchResult, get_filtered_results
 from open_webui.env import SRC_LOG_LEVELS
 
+import random
+import time
+
+import urllib.request
+import ssl
+import json
+
 log = logging.getLogger(__name__)
 log.setLevel(SRC_LOG_LEVELS["RAG"])
+
+user_agents = [
+    # Chrome 浏览器
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+    # Firefox 浏览器
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0",
+    # Edge 浏览器
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0"
+]
+
+last_request_time = 0
+
+def send_request_with_retry(
+    params: dict,
+):
+    global last_request_time
+
+    base_url = "https://www.baidu.com/s"
+
+    url = f"{base_url}?{urlencode(params)}"
+    log.info(f"searching {url}")
+
+    headers = {
+        "User-Agent": random.choice(user_agents),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-",
+        "Accept-Encoding": "gzip, deflate, zstd",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Connection": "keep-alive",
+        "Cookie": "BAIDUID=B863A09046D5D8CB6710A10ADC00C88E:FG=1; BIDUPSID=B863A09046D5D8CB6710A10ADC00C88E; PSTM=1730987767; BD_UPN=123253; BAIDUID_BFESS=B863A09046D5D8CB6710A10ADC00C88E:FG=1;"
+    }
+
+    # 获取当前时间
+    current_time = time.time()
+    # 计算距离上次请求经过的时间
+    elapsed_time = current_time - last_request_time
+    if elapsed_time < 1:
+        # 若不足 1 秒，生成 1 到 3 秒的随机等待时间
+        wait_time = random.randint(1, 3)
+        log.info(f"Wait for {wait_time} second...")
+        time.sleep(wait_time)
+        
+    response = requests.get(
+        base_url,
+        headers=headers,
+        params=params,
+        timeout=10,
+    )
+    last_request_time = time.time()
+    response.raise_for_status() # 检查请求是否成功
+
+    try:
+        data = response.json()
+        return data
+    except Exception as e:
+        log.info(f"Failed to parse response: {e}")
+        log.info(response.text)
+
+        log.info("Try to request via urllib.request")
+        context = ssl._create_unverified_context()
+        req = urllib.request.Request(url=url, headers=headers)
+        wait_time = random.randint(1, 3)
+        log.info(f"Wait for {wait_time} second...")
+        time.sleep(wait_time)
+        html = urllib.request.urlopen(req, context=context).read().decode('UTF-8')
+        last_request_time = time.time()
+
+        try:
+            data = json.loads(html)
+            return data
+        except Exception as e:
+            log.info(f"Failed to parse response: {e}")
+            log.info(html)
+            raise Exception(f"Failed to get json response")
 
 
 def search_searxng(
@@ -16,76 +100,48 @@ def search_searxng(
     filter_list: Optional[list[str]] = None,
     **kwargs,
 ) -> list[SearchResult]:
-    """
-    Search a SearXNG instance for a given query and return the results as a list of SearchResult objects.
+    
+    log.info("search via baidu")
+    
+    results = []
+    original_count = count
 
-    The function allows passing additional parameters such as language or time_range to tailor the search result.
+    page_num = 0
+    results_per_page = 15
 
-    Args:
-        query_url (str): The base URL of the SearXNG server.
-        query (str): The search term or question to find in the SearXNG database.
-        count (int): The maximum number of results to retrieve from the search.
+    while count > 0:
+        params = {
+            "wd": query,
+            "pn": page_num * results_per_page, 
+            "rn": results_per_page,
+            "tn": "json",
+        }
 
-    Keyword Args:
-        language (str): Language filter for the search results; e.g., "en-US". Defaults to an empty string.
-        safesearch (int): Safe search filter for safer web results; 0 = off, 1 = moderate, 2 = strict. Defaults to 1 (moderate).
-        time_range (str): Time range for filtering results by date; e.g., "2023-04-05..today" or "all-time". Defaults to ''.
-        categories: (Optional[list[str]]): Specific categories within which the search should be performed, defaulting to an empty string if not provided.
+        try:
+            data = send_request_with_retry(params)
+        except Exception as e:
+            raise Exception(f"Invalid response: {e}") from e
 
-    Returns:
-        list[SearchResult]: A list of SearchResults sorted by relevance score in descending order.
+        if "feed" not in data or "entry" not in data["feed"]:
+            log.info(data)
+            raise Exception("Invalid response: no feed or no entry in feed")
 
-    Raise:
-        requests.exceptions.RequestException: If a request error occurs during the search process.
-    """
+        for entry in data["feed"]["entry"]:
+            if not entry.get("title") or not entry.get("url"):
+                continue
 
-    # Default values for optional parameters are provided as empty strings or None when not specified.
-    language = kwargs.get("language", "en-US")
-    safesearch = kwargs.get("safesearch", "1")
-    time_range = kwargs.get("time_range", "")
-    categories = "".join(kwargs.get("categories", []))
+            results.append(
+                SearchResult(
+                    link=entry["url"], 
+                    title=entry["title"], 
+                    snippet=entry.get("abs", ""),
+                )
+            )
 
-    params = {
-        "q": query,
-        "format": "json",
-        "pageno": 1,
-        "safesearch": safesearch,
-        "language": language,
-        "time_range": time_range,
-        "categories": categories,
-        "theme": "simple",
-        "image_proxy": 0,
-    }
-
-    # Legacy query format
-    if "<query>" in query_url:
-        # Strip all query parameters from the URL
-        query_url = query_url.split("?")[0]
-
-    log.debug(f"searching {query_url}")
-
-    response = requests.get(
-        query_url,
-        headers={
-            "User-Agent": "Open WebUI (https://github.com/open-webui/open-webui) RAG Bot",
-            "Accept": "text/html",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Connection": "keep-alive",
-        },
-        params=params,
-    )
-
-    response.raise_for_status()  # Raise an exception for HTTP errors.
-
-    json_response = response.json()
-    results = json_response.get("results", [])
-    sorted_results = sorted(results, key=lambda x: x.get("score", 0), reverse=True)
-    if filter_list:
-        sorted_results = get_filtered_results(sorted_results, filter_list)
-    return [
-        SearchResult(
-            link=result["url"], title=result.get("title"), snippet=result.get("content")
-        )
-        for result in sorted_results[:count]
-    ]
+            if len(results) >= original_count:
+                break
+        
+        page_num += 1
+        count -= results_per_page
+    
+    return results
